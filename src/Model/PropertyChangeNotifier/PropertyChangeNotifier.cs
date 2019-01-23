f66ec41a5d9796b618a99b1daad0774e74ea196b
MetanoKid <carloscdcskate@gmail.com>
2019-01-24 00:15:46 +0100

    Adds helper base class that implements INotifyPropertyChanged

---
+++ src/Model/PropertyChangeNotifier/PropertyChangeNotifier.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace Model
+{
+    public abstract class PropertyChangeNotifier : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
