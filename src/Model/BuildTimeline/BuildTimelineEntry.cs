d47f9c22b435a51231fb4a19327f85763e06bb52
MetanoKid <carloscdcskate@gmail.com>
2019-03-26 23:29:02 +0100

    Refactor to separate stuff into their own files

---
+++ src/Model/BuildTimeline/BuildTimelineEntry.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Build.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class BuildTimelineEntry
+    {
+        public BuildEventArgs StartBuildEvent
+        {
+            get;
+            set;
+        }
+
+        public BuildEventArgs EndBuildEvent
+        {
+            get;
+            set;
+        }
+
+        public TimeSpan ElapsedTime
+        {
+            get
+            {
+                return EndBuildEvent.Timestamp - StartBuildEvent.Timestamp;
+            }
+        }
+
+        public ThreadAffinity ThreadAffinity
+        {
+            get;
+            set;
+        }
+
+        public BuildTimelineEntry Parent
+        {
+            get;
+            private set;
+        }
+
+        public List<BuildTimelineEntry> Children
+        {
+            get;
+            private set;
+        }
+
+        public List<BuildMessageEventArgs> Messages
+        {
+            get;
+            private set;
+        }
+
+        public BuildTimelineEntry()
+        {
+            ThreadAffinity = new ThreadAffinity();
+            Children = new List<BuildTimelineEntry>();
+            Messages = new List<BuildMessageEventArgs>();
+        }
+
+        public void AddChild(BuildTimelineEntry entry)
+        {
+            Children.Add(entry);
+            entry.Parent = this;
+        }
+
+        public bool OverlapsWith(BuildTimelineEntry entry)
+        {
+            return StartBuildEvent.Timestamp < entry.EndBuildEvent.Timestamp &&
+                   entry.StartBuildEvent.Timestamp < EndBuildEvent.Timestamp;
+        }
+    }
+}
---
+++ src/Model/BuildTimeline/ThreadAffinity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ThreadAffinity
+    {
+        public int ThreadId
+        {
+            get;
+            set;
+        }
+
+        public HashSet<int> InvalidThreadIds
+        {
+            get;
+            set;
+        }
+
+        public bool Calculated
+        {
+            get;
+            set;
+        }
+
+        public ThreadAffinity()
+        {
+            ThreadId = 0;
+            InvalidThreadIds = new HashSet<int>();
+            Calculated = false;
+        }
+    }
+}
